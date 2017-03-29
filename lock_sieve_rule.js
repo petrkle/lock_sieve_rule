2bf1c57fce662b8ae60ffe13255fa0955c290e19
Petr <petr@kle.cz>
2017-03-29 16:24:44 +0200

    Disable filtermenu on selected rule

--- lock_sieve_rule.js
+++ lock_sieve_rule.js
@@ -2,4 +2,16 @@ $(document).ready(function() {
 	if($('#_name').val() && $('#_name').val() == window.rcmail.env.lock_rule_name){
 		$("#footer").html('<b>' + window.rcmail.env.lock_rule_info + '</b>');
 	}
+
+	$(document).on("click", "#filtermenulink", function() {
+		$('.name').each(function() {
+			    if($(this).text() == window.rcmail.env.lock_rule_name){
+						if($(this).parent().attr('class') == 'selected focused'){
+							window.location.reload();
+						}
+					}
+		});
+
+	});
+
 });
